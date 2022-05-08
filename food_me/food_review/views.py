75dce7f2b9bfd9380f91a9d06b094f0aac450804
Allison Kroboth <42.allison.k@gmail.com>
2022-05-08 11:15:15 -0400

    completed home page view added tamplate

--- food_me/food_review/views.py
+++ food_me/food_review/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views import View
+from food_review.models import Restaurant
+
 # Create your views here.
+
+class HomePageView(View):
+    def get(self, request):
+        return render(request, "home.html")
