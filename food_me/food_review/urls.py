e2dea3683cebb33d45510d46fa0948adf36a6d3b
Blaine-Reid <reid.blaine08@gmail.com>
2022-05-16 17:28:46 -0700

    'started-search'

--- food_me/food_review/urls.py
+++ food_me/food_review/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from food_review.views import HomePageView
+from food_review.views import HomePageView, SearchView
+
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home") 
+    path("", HomePageView.as_view(), name="home"),
+    path("search/", SearchView.as_view(), name='search'),
+    # path("restaurant/", RestaurantView.as_view(), name='restaurant'),
+
 ]
--- food_me/food_review/views.py
+++ food_me/food_review/views.py
@@ -1,3 +1,4 @@
+from django.http import QueryDict
 from django.shortcuts import render
 
 from django.views import View
@@ -5,6 +6,29 @@ from food_review.models import Restaurant
 
 # Create your views here.
 
+
 class HomePageView(View):
     def get(self, request):
-        return render(request, "home.html")
+        return render(request, "index.html")
+
+
+class SearchView(View):
+
+    def get(self, request):
+
+        try:
+            search = request.GET['q']
+            restaurants = Restaurant.objects.filter(name__contains=str(search))
+
+            context = {
+                "search": search,
+                "restaurants":  restaurants
+            }
+            print(restaurants)
+            return render(request, "search.html", context=context)
+
+        except:
+            context = {
+                "search": ''
+            }
+            return render(request, "search.html", context=context)
