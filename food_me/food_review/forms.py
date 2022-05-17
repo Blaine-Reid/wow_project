56873f0d38f1350e726b15fe7791aee7993feede
Allison Kroboth <42.allison.k@gmail.com>
2022-05-17 18:31:03 -0400

    completed forms

--- food_me/food_review/forms.py
+++ food_me/food_review/forms.py
@@ -1,7 +1,23 @@
+from food_review.models import TypeOfFood, Tag
 from django import forms
+
 
 
 class AddRestaurant(forms.Form):
     """Add a restaurant"""
+    
+    resturant_name = forms.CharField(label = "Restaurant Name", max_length = 255)
+    address = forms.CharField(label = "Street Address", max_length = 255)
+    city = forms.CharField(label = "City", max_length=100)
+    state = forms.CharField(label = "State", max_length=50)
+    zip_code = forms.CharField(label = "Zip Code", max_length=5)
+    phone_number = forms.CharField(label = "Phone Number", max_length = 10)
+    type_food = forms.MultipleChoiceField( choices=[(tq.pk, tq.__str__()) for tq in TypeOfFood.objects.all()])
+    
 
-    pass
+
+
+class AddRestaurantReview(forms.Form):
+    tag = forms.MultipleChoiceField(choices = [(tq.pk, tq.__str__()) for tq in Tag.objects.all()])
+    rating = forms.IntegerField(label = "How would you rate this restaurant?", max_value = 5)
+    review = forms.CharField(label = "Your review", max_length = 500)
--- food_me/food_review/views.py
+++ food_me/food_review/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 from django.views import View
 from food_review.models import Restaurant
-
+from food_review.forms import AddRestaurant
 # Create your views here.
 
 class HomePageView(View):
     """Renders home page"""
     def get(self, request):
-        return render(request, "home.html")
+        return render(request, "index.html")
 
 class SearchView(View):
     """Renders and processes searches"""
@@ -22,7 +22,7 @@ class ReviewView(View):
 
 class AddRestaurantView(View):
     def get(self, request):
-        return render(request)
+        return render(request, "add_restaurant.html", {"AddRestaurant": AddRestaurant})
 
 class RestaurantProfileView(View):
     def get(self, request):
